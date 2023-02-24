47855d93215ad31f839e1ab316eaaec29c8291ae
aracelliQA <aracellieng@gmail.com>
2023-02-24 16:24:46 -0300

    Fixtures

--- cypress/e2e/feedback.cy.ts
+++ cypress/e2e/feedback.cy.ts
@@ -1,13 +1,17 @@
 describe('Feedback test', ()=>{
 it('Should submit feedback form', () =>{
     cy.visitFeedbackPage()
-    cy.submitFeedback(
-        'Ana',
-        'test@email.com',
-        'My subject',
-        'Message for you!'
-    )
+   // cy.submitFeedback(
+       // 'Ana',
+       // 'test@email.com',
+       // 'My subject',
+       // 'Message for you!'
 
+
+    //)
+    cy.fixture('feedbackData').then(({name, email, subject, message}) => {
+      cy.submitFeedback(name, email, subject, message)  
+    })
 })
 
 })
--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -2,7 +2,9 @@ describe('Demo Test', () => {
   it('Should visit ZeroBank Website and login an application', () => {
     cy.visitHomePage()
     cy.visitLoginPage()
-    cy.login('username','password')
-    
+    //cy.login('username','password')
+    cy.fixture("loginData").then(({username, password})=>{
+      cy.login(username,password)
+    })
   })
 })
